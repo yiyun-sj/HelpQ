3286378a25c420674480630a63d43fd7b7f3ef6d
diegodofe <diego.dorantes-ferreira@mail.mcgill.ca>
2022-09-28 00:53:48 -0400

    Configure segment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import * as snippet from '@segment/snippet'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const loadSegment = () => {
+    const options = {
+      apiKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY,
+    }
+    if (process.env.NEXT_PUBLIC_NODE_ENV) {
+      return snippet.max(options)
+    }
+    return snippet.min(options)
+  }
+  return (
+    <>
+      <Script
+        dangerouslySetInnerHTML={{ __html: loadSegment() }}
+        id='segmentScript'
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import { useEffect } from 'react'
+
 export default function Home() {
+  useEffect(() => {
+    // @ts-ignore
+    window?.analytics?.page(Component.displayName)
+  }, [])
+
   return <div>Welcome!</div>
 }
