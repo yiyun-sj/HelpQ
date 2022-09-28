0b6d9496df67ab15355cd90638bb575fae6e3a18
yiyun-sj <67709970+yiyun-sj@users.noreply.github.com>
2022-09-27 23:19:55 -0400

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,6 @@
-import { AnimatePresence, motion } from 'framer-motion'
-import Link from 'next/link'
 
 export default function Home() {
   return (
-    <AnimatePresence exitBeforeEnter>
-      <motion.div
-        initial={{ opacity: 0, x: -1000 }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: 1000 }}
-      >
-        <button type='button'>
-          <Link href='/cats' passHref>
-            <a href='replaced'>View Cats</a>
-          </Link>
-        </button>
-      </motion.div>
-    </AnimatePresence>
+    <div></div>
   )
 }
