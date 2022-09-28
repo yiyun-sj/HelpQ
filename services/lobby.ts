2899fd1e2913cf9840944ae6eae7a84947f55980
diegodofe <diego.dorantes-ferreira@mail.mcgill.ca>
2022-09-28 17:18:11 -0400

    Add create lobby function

--- services/lobby.ts
+++ services/lobby.ts
@@ -1,5 +1,12 @@
-export function createLobby() {
-  return undefined
+import { addDoc, collection, Timestamp } from 'firebase/firestore'
+import db from '../firebase'
+
+export async function createLobby() {
+  const docRef = await addDoc(collection(db, 'lobbies'), {
+    created: Timestamp.now(),
+  })
+
+  return docRef
 }
 
 export function listenToLobby() {
--- types/lobby.ts
+++ types/lobby.ts
@@ -1,3 +1,6 @@
+import { Timestamp } from 'firebase/firestore'
+
 export interface Lobby {
   id: string
+  created: Timestamp
 }
